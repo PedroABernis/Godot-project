ba83fa45b2ad6829424d2569bb9b78fb6d281ed9
PedroABernis <140460675+PedroABernis@users.noreply.github.com>
2025-11-10 01:31:05 -0300

    Comitando o Jogo

---
+++ Mr Mochi/scripts/coin.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var coins := 10
+@onready var coin_effect = $coin_effect as AudioStreamPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	$anim.play("collect")
+	coin_effect.play()
+	await $collision.call_deferred("queue_free")
+	Globals.coins += coins
+	
+
+func _on_anim_animation_finished():
+	queue_free()
